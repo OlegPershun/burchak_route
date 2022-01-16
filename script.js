d02e6bc2fa42c2b07ae97bb646551bfbdc15d89f
Oleg Pershun <49560143+OlegPershun@users.noreply.github.com>
2022-01-16 17:42:11 +0200

    v0.2

---
+++ script.js
@@ -0,0 +1,443 @@
+let dataList = {
+    "route": {
+        "0": {
+            "id": 0,
+            "type": "route",
+            "name": "Маршрут Григоріївка-Бурчак"
+        }
+    },
+    "nature": {
+        "0": {
+            "id": 0,
+            "type": "n_mount",
+            "name": "Гора Лисуха",
+            "type_ua": "Гора"
+        },
+        "1": {
+            "id": 1,
+            "type": "n_lake",
+            "name": "Озеро Бурчак",
+            "type_ua": "Озеро"
+        },
+        "2": {
+            "id": 2,
+            "type": "n_tract",
+            "name": "Урочище Грибиней",
+            "type_ua": "Урочище"
+        },
+        "3": {
+            "id": 3,
+            "type": "n_lake",
+            "name": "Озеро Ярок",
+            "type_ua": "Озеро"
+        },
+        "4": {
+            "id": 4,
+            "type": "n_mount",
+            "name": "Игнатенкова гора",
+            "type_ua": "Гора"
+        },
+        "5": {
+            "id": 5,
+            "type": "n_tract",
+            "name": "Урочище Тополина гора",
+            "type_ua": "Урочище"
+        }
+    },
+    "poi_arch": {
+        "0": {
+            "id": 0,
+            "type": "p_arch",
+            "name": "Поселення трипільської культури",
+            "p_id": 41,
+            "type_ua": "Пам'ятка археології"
+        },
+        "1": {
+            "id": 1,
+            "type": "p_arch",
+            "name": "Могильник зарубинецької культури",
+            "p_id": 42,
+            "type_ua": "Пам'ятка археології"
+        },
+        "2": {
+            "id": 2,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 43,
+            "type_ua": "Пам'ятка археології"
+        },
+        "3": {
+            "id": 3,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 44,
+            "type_ua": "Пам'ятка археології"
+        },
+        "4": {
+            "id": 4,
+            "type": "p_arch",
+            "name": null,
+            "p_id": 45,
+            "type_ua": "Пам'ятка археології"
+        },
+        "5": {
+            "id": 5,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 47,
+            "type_ua": "Пам'ятка археології"
+        },
+        "6": {
+            "id": 6,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 49,
+            "type_ua": "Пам'ятка археології"
+        },
+        "7": {
+            "id": 7,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 51,
+            "type_ua": "Пам'ятка археології"
+        },
+        "8": {
+            "id": 8,
+            "type": "p_arch",
+            "name": "Поселення багатошарове, могильник давньоруський",
+            "p_id": 52,
+            "type_ua": "Пам'ятка археології"
+        },
+        "9": {
+            "id": 9,
+            "type": "p_arch",
+            "name": "Городище давньоруське",
+            "p_id": 53,
+            "type_ua": "Пам'ятка археології"
+        },
+        "10": {
+            "id": 10,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 54,
+            "type_ua": "Пам'ятка археології"
+        },
+        "11": {
+            "id": 11,
+            "type": "p_arch",
+            "name": "Поселення зарубинецького та давньоруського часу",
+            "p_id": 55,
+            "type_ua": "Пам'ятка археології"
+        },
+        "12": {
+            "id": 12,
+            "type": "p_arch",
+            "name": "Поселення ранньозалізного віку та давньоруське",
+            "p_id": 57,
+            "type_ua": "Пам'ятка археології"
+        },
+        "13": {
+            "id": 13,
+            "type": "p_arch",
+            "name": "Поселення зарубинецької культури",
+            "p_id": 59,
+            "type_ua": "Пам'ятка археології"
+        },
+        "14": {
+            "id": 14,
+            "type": "p_arch",
+            "name": "Поселення багатошарове",
+            "p_id": 60,
+            "type_ua": "Пам'ятка археології"
+        }
+    },
+    "poi_tursm": {
+        "0": {
+            "id": "0",
+            "type": "pt_camp",
+            "name": "Обійстя Козака з Лугу Сашка",
+            "type_ua": "Послуги"
+        },
+        "1": {
+            "id": "1",
+            "type": "pt_museum",
+            "name": "Плановий Бурчацький карєзнавчий музей",
+            "type_ua": "Музей"
+        },
+        "2": {
+            "id": "2",
+            "type": "pt_well",
+            "name": "Рожена криниця",
+            "type_ua": "Криниця"
+        },
+        "3": {
+            "id": "3",
+            "type": "pt_shop",
+            "name": "Магазин",
+            "type_ua": "Магазин"
+        },
+        "4": {
+            "id": "4",
+            "type": "pt_beach",
+            "name": "Пляж",
+            "type_ua": "Пляж"
+        },
+        "5": {
+            "id": "5",
+            "type": "pt_observ",
+            "name": "Оглядовий майданчик",
+            "type_ua": "Місце"
+        },
+        "6": {
+            "id": "6",
+            "type": "pt_beach",
+            "name": "Пляж",
+            "type_ua": "Пляж"
+        },
+        "7": {
+            "id": "7",
+            "type": "pt_tank",
+            "name": "Пам'ятник танкістам Букринського плацдарму",
+            "type_ua": "Пам'ятник"
+        },
+        "8": {
+            "id": "8",
+            "type": "pt_zatul",
+            "name": "Пам'ятник В. І. Затуливітеру",
+            "type_ua": "Пам'ятник"
+        },
+        "9": {
+            "id": "9",
+            "type": "pt_mon206",
+            "name": "Пам'ятник воїнам 206-ї стрілкової дивізії",
+            "type_ua": "Пам'ятник"
+        }
+    }
+}
+
+function loadData(){
+    let poiDescription
+    fetch("./data/desc/poi_description.json").then(res => res.json()).then(data => {poiDescription = data})
+    let tursmDescription
+    fetch("./data/desc/tursm_description.json").then(res => res.json()).then(data => {tursmDescription = data})
+
+
+    fetch("./data/layers/route.geojson").then(res => res.json()).then(data => {
+        data.features.forEach((f)=>{
+            let coords = f.geometry.coordinates;
+            let cc = coords.map((item)=>{
+                return item.map((i)=>{ return i.reverse()})
+            })
+
+            L.polyline.antPath(cc, {
+                "paused": false,
+                "reverse": false,
+                "delay": 3000,
+                "dashArray": [30, 20],
+                "weight": 4,
+                "opacity": 0.6,
+                "color": "#194201",
+                "pulseColor": "#ffff"
+            }).addTo(map)
+        })
+      });
+
+    fetch("./data/layers/nature.geojson").then(res => res.json()).then(data => {
+        L.geoJSON(data, {
+            style : styles.nature
+        }).addTo(map);
+      });
+
+    // fetch("./data/layers/poi_arch.geojson").then(res => res.json()).then(data => {
+    //     L.geoJSON(data, {
+    //         style : styles.poi_arch,
+    //         pointToLayer : function(feature, latlng){
+    //             return L.marker(latlng, {
+    //                 opacity : 0,
+    //                 interactive : false})
+    //         },
+    //         onEachFeature : function(feature, layer){
+    //             // layer.on('click', function(){
+    //             //     sidebar.toggle()
+    //             // })
+    //             layer.bindPopup(`<div>
+    //                                 <h1 class='popup-poi-arch-header'>${feature.properties.name}</h1>
+    //                                 <p class='popup-poi-arch-description'> ${poiDescription[feature.properties.id]}</p>
+    //                             </div>`,
+    //                                 {
+    //                                     maxWidth : '600'
+    //                                 })
+    //             layer.bindTooltip(feature.properties.name, {
+    //                 permanent : true, 
+    //                 className : "labelstyle", 
+    //                 direction : "top",
+    //                 interactive : true
+    //             })
+    //         }
+    //     }).addTo(map);
+    //   });
+
+    fetch("./data/layers/poi_tursm.geojson").then(res => res.json()).then(data => {
+
+        L.geoJSON(data, {
+            style : styles.poi_tursm,
+            pointToLayer : function(feature, latlng){
+                return L.marker(latlng, {
+                    icon : L.icon({
+                        iconUrl: `./data/icons/${feature.properties.type}.svg`,
+                        iconSize: [20, 50]
+                    })
+                })
+            },
+            onEachFeature : function(feature, layer){
+                layer.bindTooltip(feature.properties.name, {
+                    direction : 'top',
+                    offset : [0, -15]
+                })
+
+                layer.on('click', function(e){
+                    sidebar.open('current')
+                    document.querySelector('#current > .leaflet-sidebar-header').innerHTML = feature.properties.name
+                    document.querySelector('#current > .title-pane').innerHTML = `<img class="poi_img" width="520" src="./data/img/pois/poi_tursm/${feature.properties.id}.jpg">` + tursmDescription[feature.properties.type]
+                })
+
+                // layer.on('mouseover', function(e){
+                //     e.target.setIcon(new icon({
+                //         shadowUrl: './data/icons/shadow_green.png',
+                //         shadowSize: [50, 50]
+
+                //     }))
+                //     layer.openTooltip()
+                // })
+                // layer.on('mouseout', function(e){
+                //     e.target.setIcon(new icon({
+                //         shadowUrl: './data/icons/shadow.png',
+                //         shadowSize: [50, 50]
+                //     }))
+                // })
+            }
+        }).addTo(map);
+      });
+}
+loadData()
+
+const config = {
+    apikey : 'AAPK635bb11113274d178e965eed8f6c4cfaHJFQ_TpKbTkls-5IQV19gn3CYsZxtvEMnHnrswUgs2cgOvG1OAeLek-cQCdgGYP9'
+}
+
+const styles = {
+    route : {
+        'color' : '#04ff00',
+        'opacity' : 0.5
+    },
+    nature : {
+        'stroke' : false,
+        'fillOpacity' : 0,
+        'interactive' : false
+    },
+    poi_arch : {
+        'interactive' : false
+    },
+    poi_tursm : {
+        'stroke' : false,
+        'fillOpacity' : 0,
+    },
+    monuments : {
+        
+    }
+}
+
+const basemaps = {
+    OSM : L.esri.Vector.vectorBasemapLayer('OSM:StandardRelief', {apikey: config.apikey}),
+    Imagery : L.esri.Vector.vectorBasemapLayer('ArcGIS:Imagery', {apikey: config.apikey}),
+}
+
+let map = L.map('map', {layers : basemaps.Imagery}).setView([49.901910, 31.429081], 13);
+
+let control = L.control.layers(basemaps, null, {collapsed : true, position : 'topleft'}).addTo(map)
+
+const sidebar = L.control.sidebar({
+    autopan: true,    
+    container: 'sidebar', 
+    position: 'left',    
+}).addTo(map);   
+
+sidebar.addPanel({
+    id: 'home',              
+    tab: '<i class="fas fa-home"></i>',  
+    pane : '<p id="home-title-h1"><b>ТРАХТЕМИРІВСЬКА ЛÝКА - СЕРЦЕ КАНІВСЬКИХ ГІР</b></p>' +
+        '<div class="pane-wrapper"><p><b>Довжина маршруту</b> - 11 км</p>' +
+        '<p><b>Перепад висот</b> – не більше 150м</p>' +
+        '<p><b>Середня тривалість пересування</b> – 4-6 год в залежності від підготованості та вікової категорії туристів.</p>' +
+        '<p><b>Опис маршруту</b> - пішохідний пізнавальний маршрут проходить центральною частиною Канівських гір в межах Трахтемирівського півострова або луки (дугоподібного вигину ріки чи берега) та представляє найбільш характерні природні утворення цієї місцевості - поєднує лісову та лучну ділянки вздовж Дніпра. Головним об’єктом уваги є особливий ландшафт цієї місцевості. Канівські гори відомі в геологічній науці як Канівські дислокації і вирізняються унікальною геологічною будовою. Вона визначила специфічність ландшафту Канівських гір – вони вкриті густою та розгалуженою яружно-балковою мережею. Тут проходить найпівденніша ділянка природних грабово-дубових лісів (їх прикладом є Голосіївський ліс у Києві). Тут зафіксовано близько 15 червонокнижних видів флори та фауни. Половина маршруту проходить через лісовий масив Бучацька дача, друга половина – через лучну прибережну ділянку, яка подекуди включає старий фруктовий сад. По маршруту розташовано 19 археологічних пам’яток, що представляють скіфську, зарубинецьку, давньоруську та пізньосередньовічну епохи. На початку маршруту з боку села Бучак знаходиться збережений вітряк (1946 рік), братська могила воїнів Букринського плацдарму, а також створюваний Бучацький краєзнавчий музей. На маршруті зустрічаються ряд місцевих джерел (вода питна), а також гідрологічна пам’ятка Рожена криниця. На маршруті, окрім сіл Григорівка та Бучак, відсутні інші населені пункти, будинки та споруди. У селі Григорівка є нова, відбудована у козацькому стилі Миколаївська церква, також працюють два магазини. Перед Григорівкою на постаменті встановлено танк Т-34 на честь форсування Букринського плацдарму у ІІ-ї Світовій війні. Загалом це надзвичайно мальовнича місцевість. Маршрут розрахований на звичайних туристів без підготовки.</p></div>',
+    title: 'Опис маршруту',             
+    position: 'top'                  
+});
+
+sidebar.addPanel({
+    id: 'current',                   
+    tab: '<i class="fas fa-crosshairs"></i>',  
+    pane : '<p class="title-pane">Оберіть місце інтересу на карті</p>',
+    title: 'Обраний об’єкт',             
+    position: 'top'                    
+});
+
+
+sidebar.addPanel({
+    id: 'list',                   
+    tab: '<i class="fas fa-list-ul"></i>',  
+    pane : `<p class="title-pane">generateList()</p>`,
+    title: 'List of POIs',             
+    position: 'top'                    
+});
+sidebar.open('home')
+
+function generateList(data){
+    let result = document.createElement('div')
+        result.id = 'wrapper'
+
+    for(let item in data){
+        console.log(item)
+        a.push(`<a href="" class="dropdown-item-f">${item}</a>
+                <ul class="dropdown-list-s">`)
+        for(let item2 in data[item]){
+            console.dir(`---${data[item][item2].name}`)
+            a.push(`<li>
+                        <a href="" class="dropdown-item-s">${data[item][item2].name}</a>
+                    </li>`)
+        }
+    }
+
+
+
+   `
+    <div id="wrapper">
+         <nav class="dropdown-list">
+            <li>
+            
+    
+    
+    `
+    // `<div id="wrapper">
+    //     <nav class="dropdown-list">
+    //         <li>
+    //             <a href="" class="dropdown-item-f"></a>
+    //             <ul class="dropdown-list-s">
+    //                 <li>
+    //                     <a href="" class="dropdown-item-s">
+    //                 </li>
+    //             </ul>
+    //         </li>
+    //     </nav>
+    // </div>`
+
+    console.log(a)
+}
+
+let result = document.createElement('div')
+result.id = 'wrapper'
+result.innerHTML = `<li></li>`
+
+console.log(result)
+
+
